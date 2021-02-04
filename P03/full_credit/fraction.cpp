557c6170c697eb242b9c03f2cb960c613a628109
Ellis Nielsen <thecakebecamethelie@yahoo.com>
2021-02-04 17:30:42 -0600

    added operators -,*,/

--- P03/full_credit/fraction.cpp
+++ P03/full_credit/fraction.cpp
@@ -11,21 +11,56 @@ Fraction::Fraction(int numerator = 0, int denominator = 1): _numerator{numerator
 
 //Fraction Fraction::operator-();
 
-/*This function will add both fractions
-Formula used is a/b + c/d = (ad+bc)/bd */
+/*
+This operator will add both fractions
+Formula used is a/b + c/d = (ad+bc)/bd 
+*/
 Fraction Fraction::operator+(Fraction& rhs){
-    /*Numerator*/
-    int tempNum = (*this._numerator * rhs._denominator) + (*this->_denominator * rhs._numerator);   
-    /*Denominator*/
-    int tempDem = *this._denominator * rhs._denominator;                                           
-    Fraction retFraction{tempNum,tempDem};
+    /*Numerator: ad+bc*/
+    int tempNum = (*this._numerator * rhs._denominator) + (*this._denominator * rhs._numerator);   
+    /*Denominator: bd*/
+    int tempDen = *this._denominator * rhs._denominator;                                           
+    Fraction retFraction{tempNum,tempDen};
     return retFraction;
 }
-Fraction Fraction::operator-(Fraction& rhs){
 
+/*
+This operator will sub both fractions
+Formula used is a/b - c/d = (ad-cb)/bd
+*/
+Fraction Fraction::operator-(Fraction& rhs){
+    /*Numerator: ad-cb*/
+    int tempNum = (*this._numerator * rhs._denominator) - (*this._denominator * rhs._numerator);
+    /*Denominator: bd*/
+    int tempDen = *this._denominator * rhs._denominator;
+    Fraction retFraction{tempNum,tempDen};
+    return retFraction;
 }
-Fraction Fraction::operator*(Fraction& rhs){}
-Fraction Fraction::operator/(Fraction& rhs){}
+
+/*
+This operator will muti both fractions
+Formula used is a/b * c/d = ac/bd
+*/
+Fraction Fraction::operator*(Fraction& rhs){
+    /*Numerator: ac*/
+    int tempNum = *this._numerator * rhs._numerator;
+    /*Denominator: bd*/
+    int tempDen = *this._denominator * rhs._denominator;
+    Fraction retFraction{tempNum,tempDen};
+    return retFraction;
+}
+/*
+This operator will divide both fractions
+Formula used is a/b / c/d = ad/bc
+*/
+Fraction Fraction::operator/(Fraction& rhs){
+    /*Numerator: ad*/
+    int tempNum = *this._numerator * rhs._denominator;
+    /*Denominator: bc*/
+    int tempDen = *this._denominator * rhs._numerator;
+    Fraction retFraction{tempNum,tempDen};
+    return retFraction;
+}
 
 friend ostream& operator<<(ostream& ost, Fraction& m){}
 friend istream& operator>>(istream& ist, Fraction& m){}
