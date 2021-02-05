144566daf26c5942f66e963f043b0e4e84366c8e
Ellis Nielsen <thecakebecamethelie@yahoo.com>
2021-02-04 18:10:40 -0600

    add all friend methods

--- P03/full_credit/fraction.cpp
+++ P03/full_credit/fraction.cpp
@@ -49,6 +49,7 @@ Fraction Fraction::operator*(Fraction& rhs){
     Fraction retFraction{tempNum,tempDen};
     return retFraction;
 }
+
 /*
 This operator will divide both fractions
 Formula used is a/b / c/d = ad/bc
@@ -62,6 +63,41 @@ Fraction Fraction::operator/(Fraction& rhs){
     return retFraction;
 }
 
-friend ostream& operator<<(ostream& ost, Fraction& m){}
-friend istream& operator>>(istream& ist, Fraction& m){}
-friend bool operator!=(const Fraction& lhs, const Fraction& rhs){}
+/*
+This operator will print out the fraction with a / in the middle
+it converts the numerator first into a string and then does the same to
+the denominator
+*/
+friend ostream& operator<<(std::ostream& ost, Fraction& m){
+    ost << std::to_string(m._numerator) << "/" << std::to_string(m._denominator);
+    return ost;    
+}
+
+/*
+This operator will take user input and place the numerator in the numerator
+then it places the denominator and places the denominator.
+The operator work by finding the / and then substrings before and after to get 
+the values of both numerator and denominator. Later the substrings are converted 
+to integers with stoi.
+*/
+friend istream& operator>>(std::istream& ist, Fraction& m){
+    std::string temp;
+    ist >> temp;
+    int postion = temp.find("/");
+    m._numerator = std::stoi(temp.substr(postion-1,1));
+    m._denominator = std::stoi(temp.substr(postion+1,1));
+    return ist;
+
+}
+/*
+This operator will determine if the two fractions are not equal
+and return 1 if both are not equal and 0 if they are both equal.
+operator uses a if statement to make comparison 
+*/
+friend bool operator!=(const Fraction& lhs, const Fraction& rhs){
+    if(lhs._numerator != rhs._numerator || lhs._denominator != rhs._numerator){
+        return 1;
+    }else{
+        return 0;
+    }
+}
