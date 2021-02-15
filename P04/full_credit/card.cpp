034cc2342fafffd26a898867fba970c46bcbcbfc
Ellis Nielsen <ellis.nielsen@mavs.uta.edu>
2021-02-14 21:44:22 -0600

    Added makefile, fixed errors, and finished main.cpp

--- P04/full_credit/card.cpp
+++ P04/full_credit/card.cpp
@@ -1,19 +1,31 @@
 #include "card.h"
-
+/*
+	take a question and answer in string
+	initialization list to inintalize the class
+*/
 Card::Card(std::string question, std::string answer):_question{question},_answer{answer}{}
 
 
-
+/*
+	take a string
+	compare the string to answer
+	if same return correct
+	else return not correct
+*/
 std::string Card:: attempt(std::string response){
 	if(_answer.compare(response)==0){
 		return "Correct Answer";
 	}else{
-		return "Incorrect Answer";
+		return "X - Correct answer was" + _answer + "\n";
 	}
 }
 
 
-
+/*
+	streaming out operator
+	get a stream and a card
+	insert into ostream the string of card	
+*/
 std::ostream& operator<<(std::ostream& ost, Card& m){
 	ost << m._question;
 	return ost;
--- P04/full_credit/deck.cpp
+++ P04/full_credit/deck.cpp
@@ -1,21 +1,40 @@
 #include "deck.h"
-
+/*
+	take a question and an answer and use 
+	a card to hold that info
+	add 1 to the next card
+	and shuffle the cards
+*/
 void Deck::add_card(std::string question, std::string answer){
 	_cards.push_back(Card{question,answer});
 	_options.push_back(answer);
 	++_next_card;
-	unsigned int seed = time(NULL);
+	unsigned int seed = std::time(NULL);
 	shuffle(_cards.begin(), _cards.end(), std::default_random_engine(seed));
 }
-
+/*
+	add a false answer to the vector of 
+	_options
+*/
 void Deck::add_false_answer(std::string false_answer){
 	_options.push_back(false_answer);
 }
 
+/*
+	return the vector of strings
+	that contain the options to
+	select
+*/
 std::vector<std::string> Deck::options(){
 	return _options;
 }
 
+/*
+	if the next card is 1 return the last card
+	if the card is 0 throw error
+	else subtract one from next card
+	and return _cards
+*/
 Card& Deck::deal(){
 	if(_next_card == 1){
 		return _cards[0];
